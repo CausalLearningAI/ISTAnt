5c7f06561f4b1acd26cda80253fd2eed819308ea
riccardocadei <riccardo.cadei98@gmail.com>
2024-03-13 14:13:58 +0100

    add set seed

---
+++ src/utils.py
@@ -0,0 +1,8 @@
+import torch
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
